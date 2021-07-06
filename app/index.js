4a6a49123f820567e0ea66a11c6adcd1b38918b6
Sofiane ZERNENOU <szernenou@hotmail.com>
2021-07-06 16:36:17 +0100

    Creating the Popular navbar. Small styling of the element

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/popular'
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                Hello Sofiane !
+            <div className='container'>
+                <Popular />
             </div>
         )
     }

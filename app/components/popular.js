49dca7d77e80694f8e1ae3880511331447868a4c
Sofiane ZERNENOU <szernenou@hotmail.com>
2021-07-07 12:00:27 +0100

    Adding the state to the component and updating the language value

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.selectLanguage = this.selectLanguage.bind(this);
+    }
+
+    selectLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        });
+        console.log(selectedLanguage);
+    }
+
     render() {
-        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+        const languages= ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 
         return(
             <ul className='flex-center'>
                 {languages.map((language, index) => (
                     <li key={index}>
-                        <button className='btn-clear nav-link'>{language}</button>
+                        <button className='btn-clear nav-link' onClick={() => this.selectLanguage(language)}>{language}</button>
                     </li>
                 ))}
             </ul>
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/popular'
+import Popular from './components/Popular'
 
 class App extends React.Component {
     render() {
